13b8fd621c9dc59728f51b277a83d81fe7843f2d
subhasish210 <subhasishsahu75@gmail.com>
2021-09-09 20:57:22 +0530

    first commit

---
+++ Day4_UC3_Solutions/Day4_UC3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day4_UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_EmployeeAttendance.GetAttendance();
+            UC2_EmpWage.EmpWage();
+            UC3_PartTimeEmpWage.PartTimeEmpWage();
+            UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("abc", 4, 5, 7);
+            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
